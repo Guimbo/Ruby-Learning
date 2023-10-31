2c627347174cf0b9bf67df494638df006dad70d4
mathrews <mateusps4gg@gmail.com>
2023-10-31 17:46:08 -0300

    decrypt func adicionada

--- hello.rb
+++ hello.rb
@@ -1,7 +1,15 @@
 def encrypt(string, key)
-    string_to_ascii_array = string.chars.map {|char| char.ord}
-    shifted = string_to_ascii_array.map {|char| char+key}
+    string_to_ascii_array = string.chars.map { |char| char.ord}
+    shifted = string_to_ascii_array.map { |char| char+key}
     print shifted.map { |char| char.chr }.join
 end
 
 encrypt("Hacktoberfest", 1)
+
+def decrypt(string, key)
+    string_to_ascii_array = string.chars.map { |char| char.ord}
+    shifted = string_to_ascii_array.map { |char| char - key}
+    print shifted.map { |char| char.chr }.join
+end
+
+decrypt("//Ibdlupcfsgftu//", 1)
